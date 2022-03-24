837bdac8546e94bcbfba537152afa3479e605158
Sony Kurian <67534990+sonykurian96@users.noreply.github.com>
2022-03-24 15:51:50 +0530

    Add files via upload

---
+++ src/app.config.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyAj0XwVLZmPZKBPxtt2FiqC5MnQbEEZOvw',  
+    authDomain: 'slack-clone-34543.firebaseapp.com',
+    // databaseURL: 'xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx',
+    projectId: 'slack-clone-34543',
+    storageBucket: 'slack-clone-34543.appspot.com',
+    messagingSenderId: '235418569207',
+    appId: '1:235418569207:web:d0f0e1547f56c34abd46f2',
+    // measurementId: 'xxx-xxx-xxx',
+}
+
+const cometChat = {
+  APP_ID: '206016dc084a5044',
+  AUTH_KEY: '705e318c06973db3ffcae7370699670a8b4de3d1',
+  REST_KEY: 'fe5c247904997cc2d4ce0b25e1f3e9099b5a8689',
+  APP_REGION: 'us',
+}
+
+export { firebaseConfig, cometChat }
